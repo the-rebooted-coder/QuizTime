29d31668afe94d32c0fba8466f13cdfdcb86aea2
Spandan Saxena <spandansaxena@gmail.com>
2022-01-26 11:23:46 +0530

    Update server.js

--- QuizTime/backend/src/server.js
+++ QuizTime/backend/src/server.js
@@ -18,6 +18,15 @@ app.use('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 // Listening to APIs
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+/*
 app.listen(process.env.PORT || 8000, () =>
 	console.log('Listening on Port 8000')
 )
+
+app.listen(process.env.PORT || 3000, () => { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); 
+					   });
+/*
